d4b35c4a4d5822df56327fd09dd42700bc32ecb2
ThadshajinyK <kaneshanthadshajiny@gmail.com>
2024-03-25 11:00:22 +0530

    Create Teacher schema and implement GET and POST methods

--- index.js
+++ index.js
@@ -1,84 +1,163 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+mongoose.connect("mongodb://localhost:27017/Timmins");
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-
-mongoose.connect("mongodb://localhost:27017/Timmins")
-
-const courseSchema = new mongoose.Schema(
+/** import model */
+const courseSchema = new mongoose.Schema({
+  courseName: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  courseImg: String,
+  enrolledStudents: [
     {
-        courseName: {
-            type: String,
-            required: true
-        },
-        description:{
-            type: String,
-            required: true
-        },
-        courseImg: String,
-        enrolledStudents: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Students'
-        }],
-        startDate: Date,
-        duration: String, 
-        courseFee: Number,
-        taughtBy: [
-            {
-                type: mongoose.Schema.type.ObjectId,
-                ref: 'Teachers'
-            }
-        ]
-        
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Students",
+    },
+  ],
+  startDate: Date,
+  duration: String,
+  courseFee: Number,
+  taughtBy: [
+    {
+      type: mongoose.Schema.type.ObjectId,
+      ref: "Teachers",
+    },
+  ],
+});
 
-        })
-
-        const Course = mongoose.model('Courses', courseSchema)
+const Courses = mongoose.model("Courses", courseSchema);
 
 const studentSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  dob: Date,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNo: String,
+  address: String,
+  enrolledStudents: [
+    {
+      type: mongoose.Schema.type.ObjectId,
+      ref: "Courses",
     },
-    lastName: {
-        type: String, 
-        required: true
-    },
-    dob: Date,
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    phoneNo: String,
-    address: String, 
-    enrolledStudents: [
-        {
-            type: mongoose.Schema.type.ObjectId,
-            ref: 'Courses'
-        }
-    ],
-    StudentImg: String,
-    password: {
-        type:  String,
-        required: true
-    }
-})
+  ],
+  StudentImg: String,
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-const Students = mongoose.model('Students', studentSchema)
+const Students = mongoose.model("Students", studentSchema);
 
+const teachersSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
 
+const Teachers = mongoose.model("Teachers", teachersSchema);
 
+/** HTTP methods */
 
+//GET Methods
+app.get("/getCourses", (req, res) => {
+  try {
+    Courses.find({}).then(function (response) {
+      res.json(response);
+    });
+  } catch (error) {
+    console.log("Error fetching Courses records");
+    console.error(error);
+  }
+});
 
+app.get("/getStudents", (req, res) => {
+  try {
+    Students.find({}).then(function (response) {
+      res.json(response);
+    });
+  } catch (error) {
+    console.log("Error fetching Students records");
+    console.log(error);
+  }
+});
 
+app.get("/getTeachers", (req, res) => {
+  try {
+    Teachers.find({}).then(function (response) {
+      res.json(response);
+    });
+  } catch (error) {
+    console.log("Error fetching Teachers records");
+    console.log(error);
+  }
+});
 
+// POST methods
 
+app.post("/postCourse", async (req, res) => {
+  const newCourse = new Courses(req.body);
 
+  try {
+    await newCourse.save();
+    res.json(newCourse);
+  } catch (error) {
+    console.log("Error posting course data");
+  }
+});
 
+app.post("/postStudent", async (req, res) => {
+  const newStudent = new Students(req.body);
 
+  try {
+    await newStudent.save();
+    res.json(newStudent);
+  } catch (error) {
+    console.log("Error posting student data");
+  }
+});
+
+app.post("/postTeacher", async (req, res) => {
+  const newTeacher = new Teachers(req.body);
+  try {
+    await newTeacher.save();
+    res.json(newTeacher);
+  } catch (error) {
+    console.log("Error posting teacher data");
+  }
+});
+
+app.listen("3002", () => {
+  console.log("server is running");
+});
